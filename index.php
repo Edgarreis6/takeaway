32c479e341fe4f24c6579293bc61df355a24a483
Edgar Reis <edgareis@gmail.com>
2021-06-28 23:44:14 +0100

    erros detectados
    Foram detetados erros e feitas as correções dos mesmos

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ $controller = "home";
 
 /* white list de controllers*/
 
-$valid_controllers = ["home"]
+$valid_controllers = ["home"];
 
-if(isset($_GET["controller"]) && in array($_GET["controller"], $valid_controllers) 
+if(isset($_GET["controller"]) && in_array($_GET["controller"], $valid_controllers) 
 ) {
     $controller = $_GET["controller"];
 }
 
-require("controllers/".$controller.".php");
+require("controllers/" . $controller . ".php");

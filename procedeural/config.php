e4b90712974b065072e5e9d64ee4353ca79f25cd
Edgar Reis <edgareis@gmail.com>
2021-07-26 21:08:36 +0100

    Removida pasta procedeural

--- procedeural/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-$db = new PDO("mysql:host=localhost;dbname=takeaway;charset=utf8mb4", "root", "");
-
--- procedeural/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include("config.php");
-
-unset($_SESSION["user_id"]);
-
-session_destroy();
-
-header("Location: ./");
